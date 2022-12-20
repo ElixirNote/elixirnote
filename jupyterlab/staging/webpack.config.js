b3552588543ceb4e259ab182560b94577bf4832c
ciusji <bqjimaster@gmail.com>
2022-12-20 15:29:50 +0800

    Publish 4.0.0a28
    SHA256 hashes:
    
    elixirnote-4.0.0a28-py3-none-any.whl: 66c56f4c1cc9ee409a19c4ca26af11d4a9e457b5d83cee186b03f1fd600f0f50
    
    elixirnote-4.0.0a28.tar.gz: dae157c0a0c20c4ed358ba14deaa4ccfbfa524ee7e5936f1f4379d9c651913b2

--- jupyterlab/staging/webpack.config.js
+++ jupyterlab/staging/webpack.config.js
@@ -257,7 +257,7 @@ const plugins = [
   new HtmlWebpackPlugin({
     chunksSortMode: 'none',
     template: path.join(__dirname, 'templates', 'template.html'),
-    title: jlab.name || 'ElixirNote'
+    title: jlab.name || 'JupyterLab'
   }),
   // custom plugin for ignoring files during a `--watch` build
   new WPPlugin.FilterWatchIgnorePlugin(ignored),
